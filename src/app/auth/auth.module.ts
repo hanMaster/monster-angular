6a7acc4f3800f9f6dfba8e5a7e57591bd230613c
Andrey Khalepov <w54661c@gmail.com>
2020-09-02 00:04:41 +1000

    Initial commit

---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,34 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {RouterModule, Routes} from '@angular/router';
+import {ReactiveFormsModule} from '@angular/forms';
+import {StoreModule} from '@ngrx/store';
+import {EffectsModule} from '@ngrx/effects';
+
+import {RegisterComponent} from './components/register/register.component';
+import {reducers} from './store/reducers';
+import {AuthService} from './services/auth.service';
+import {RegisterEffect} from './store/effects/register.effect';
+import {BackendErrorMessagesModule} from '../shared/modules/backendErorMessages/backendErrorMessages.module';
+
+const routes: Routes = [
+  {
+    path: 'register', component: RegisterComponent
+  }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes),
+    ReactiveFormsModule,
+    StoreModule.forFeature('auth', reducers),
+    EffectsModule.forFeature([RegisterEffect]),
+    BackendErrorMessagesModule
+  ],
+  declarations: [RegisterComponent],
+  providers: [AuthService]
+})
+
+export class AuthModule {
+}
---
+++ src/app/auth/components/register/register.component.ts
@@ -0,0 +1,48 @@
+import {Component, OnInit} from '@angular/core';
+import {Observable} from 'rxjs';
+import {select, Store} from '@ngrx/store';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+
+import {registerAction} from '../../store/actions/register.action';
+import {isSubmittingSelector, validationErrorsSelector} from '../../store/selectors';
+import {RegisterRequestInterface} from '../../types/registerRequest.interface';
+import {BackendErrorsInterface} from '../../../shared/types/backendErrors.interface';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.scss']
+})
+export class RegisterComponent implements OnInit {
+  form: FormGroup;
+  isSubmitting$: Observable<boolean>;
+  backendErrors$: Observable<BackendErrorsInterface | null>;
+
+  constructor(private fb: FormBuilder, private store: Store) {
+  }
+
+  ngOnInit(): void {
+    this.initializeForm();
+    this.initializeValues();
+  }
+
+  private initializeForm(): void {
+    this.form = this.fb.group({
+      username: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  submit(): void {
+    const request: RegisterRequestInterface = {
+      user: this.form.value
+    };
+    this.store.dispatch(registerAction({request}));
+  }
+
+  private initializeValues(): void {
+    this.isSubmitting$ = this.store.pipe(select(isSubmittingSelector));
+    this.backendErrors$ = this.store.pipe(select(validationErrorsSelector));
+  }
+}
